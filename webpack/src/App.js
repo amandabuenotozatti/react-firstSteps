ed94b4de0762441acd9dc6b0391b28b9a1bc0869
Amanda <amandabuenotozatti@gmail.com>
2022-02-24 23:55:26 -0300

    Webpack configuration

---
+++ webpack/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <div>Webpack 4</div>
+    </div>
+  );
+};
+export default App;
